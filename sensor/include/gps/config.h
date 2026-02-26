1a543840f1e8f313f09d84ac8eac9acd56449f9d
le0hr <lev.hrabovij@gmail.com>
2026-02-26 22:45:16 +0200

    test

--- sensor/include/gps/config.h
+++ sensor/include/gps/config.h
@@ -3,8 +3,8 @@
 
 #define WIFI_SSID "Lev"
 #define WIFI_PASSWORD "12345678"
-#define MQTT_BROKERS_IP "http://192.168.0.200/broker"
-#define MQTT_BROKERS_PORT 2008
+#define MQTT_BROKERS_IP "167.235.242.142"
+#define MQTT_BROKERS_PORT 1883
 #define MQTT_USER "Esp8266"
 #define MQTT_PASWORD "12345678"
 #define DEVICE_NAME "Esp8266"
